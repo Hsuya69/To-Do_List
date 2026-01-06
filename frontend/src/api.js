7352e49470bfca0a6f88515ed476b04664f2c7e6
Ayush <sinhaayush264@gmail.com>
2026-01-06 21:14:22 +0530

    updated models,users and api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const api= axios.create({baseURL: import.meta.env.VITE_API_URL
+const api= axios.create({baseURL: import.meta.env.VITE_API_URL,
+    withcredentials: true,
 });
 
 export default api
